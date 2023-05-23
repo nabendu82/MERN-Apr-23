8b96b18c1226b14048fbacba0acde566243188ae
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-05-23 10:00:28 +0530

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,81 @@
+//what js? - where i can js
+//debugg 
+
+
+//js - javascript - client  side scripting lang;
+
+//clinet side side:
+
+
+//website / webapp - 
+
+// clinent side work  -  server side work;
+
+//frontend   - backend developer;
+
+// UI - work   - local part ; server - database; poragm
+
+
+// blog  Ui - sign in  - servre 0 datatbase
+
+
+//1995 - frontend - manipulate html and css:
+
+
+//js - browser - input - html css;
+
+
+//frontend - Basic of Javascript: DOM manipulation;
+//learn react - 
+//Async 
+
+
+//1 st Basic of javascript;
+
+//Variable - is a container that will store data;
+
+//Declare variables:
+
+// keywords , identifiers , value.
+
+// keywords  - reserved words ;
+//var let const 
+
+//identifer - shpuld not statrtwith numbers;
+//  avodi , special characters - $_ - CaseSenstive;
+
+// /data - value - 
+
+
+
+
+// const personName ='Hari';
+
+// //reassigning Values:
+// // personName = 'Vikram'
+
+// console.log( personName )
+
+
+//data types: tommorrow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// why we are not using var.
+
