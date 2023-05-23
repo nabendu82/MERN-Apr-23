ed5c198a2a502d543e4b3e0bdb6cd1fc96050fba
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-05-24 02:01:54 +0530

    Add files via upload

---
+++ New folder/day2.js
@@ -0,0 +1,132 @@
+//datatypes: 
+
+//primitive:()
+//string, number , boolean , null , undefiend, BigInt , Symbol
+
+
+//reference:()
+// objects -> Array
+
+//String - squec. charactersc
+// `` '' ""
+
+// typeof
+
+// let str = '700';
+
+// console.log(typeof str)
+
+//number - inter , float;
+
+// let num = 1.900;
+
+// console.log(typeof num)
+
+//booleas - true and false;
+
+// let bool = true;
+
+// console.log(typeof bool)
+
+
+//null-BY PURPOSLY MAKE THE VALUES;
+
+// let str1 = null;
+// console.log(typeof str1);
+
+//undefined:
+
+// let a;//temporary zone
+
+// console.log(typeof a)
+
+
+// let a = Symbol(10);
+
+// let b = Symbol(10);
+
+// console.log(a === b);
+
+
+
+//object 
+
+//Array :
+
+// const arr = ['Hello',45,null,undefined,[1,2,3]]
+
+// console.log(arr[4][2])
+
+
+//objects:
+
+//order - not import;
+//numbered - named indexes;
+
+//2 things obj - properties , methods - behavouir
+
+
+const person ={
+    // key:value  - properties:
+   name:'John',
+   age:'45',
+   skils:
+['Html','css',
+   {
+    ex:5,
+    role:5
+   }
+],
+
+//METHODS
+
+details:function(){
+  return person.name + " "+person.age 
+}
+}
+
+
+console.log()
+person.details()
+
+// // console.log(person.skils[2].role)
+// let a = 'John';
+// let b = 'Welcome '
+
+// //function: 
+
+// // DECLARATION:
+// function firstPerson(greet,name){
+//     // console.log(greet,name)
+//    return  greet + name
+// }
+
+// //FUNCTION INVOCATION
+// console.log(firstPerson(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Advance - difference - refernce and primitve.
