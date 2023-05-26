8732a4793c19d271690dcdf5e3f2e99c753d2d5e
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-05-26 21:42:12 +0530

    Add files via upload

---
+++ New folder/day4.js
@@ -0,0 +1,72 @@
+//switch:
+
+
+// let color = 'yellow';
+
+
+// switch(color){
+//     case  'red':
+//         console.log('stop');
+//         break;
+//     case  'yellow':
+//             console.log('Ready');
+//             break;
+//     case 'green':
+//         console.log('Go');
+//         break;
+//     default: 
+//       console.log('Error:Unknow Color: ' +color)       
+// }
+
+
+//Loops: if i want run a statement repeatly for a n numbers times;
+
+
+
+
+//for while do-while:
+// console.log('start)
+
+//infinte  - 10;
+
+
+// for(let i =0;i<10;i++){
+//     console.log('start'+i)
+// }
+
+
+//while?
+
+// let i =10;
+
+// while(i<10){
+//     console.log('Bye');
+
+//     i++;
+// }
+
+// let i =100
+
+// do{
+// console.log('Hi');
+// i++;
+// }while(i<10);
+
+
+//iteration concepts:
+
+// 2 iteralble objects - string , array - 
+
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+
+
+// console.log(arr)
+
+
+// let i =0;
+
+
+// while(i<arr.length){
+//     console.log(arr[i]-0.5)
+//     i++;
+// }
