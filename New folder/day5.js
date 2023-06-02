ea98d9b88ef22e7c16362c267c8f56241555ff21
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-06-02 13:38:15 +0530

    Add files via upload

---
+++ New folder/day5.js
@@ -0,0 +1,72 @@
+// //Social Apps:
+
+// //connect your backend: Api - 
+
+
+// // server application - data - database  - 
+// //Api - Links -endpoints
+
+
+// const data =[
+//     {
+//         id: 6,
+//         title: "Solid Gold Petite Micropave ",
+//         price: 168,
+//         description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+        
+//     },
+//     {
+//         id: 5,
+//         title: " Petite Micropave ",
+//         price: 168,
+//         description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+//     },
+//     {
+//         id: 1,
+//         title: " Gold Petite Micropave ",
+//         price: 168,
+//         description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+//     },
+
+//     {
+//         id: 3,
+//         title: "Micropave ",
+//         price: 168,
+//         description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+//     },
+//     {id:17,
+//     title:"Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+//     price:39.99,
+//     description:"Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper "
+//   }
+
+// ]
+
+// const name="Nabendu"
+// const cont = document.getElementById('container');
+
+// console.log()
+// // cont.innerHTML = '<h1>'++'</h1>';
+
+// let i =0;
+
+// while(i<data.length){
+
+//   if(i<3){
+//     cont.innerHTML += ` <div class='inner'>
+//     <h1>${data[i].title}</h1>
+//     <p>${data[i].description}</p>
+//     <h5>${data[i].price}</h5>
+//     </div>
+//    `
+//     console.log('hi')
+//   }
+    
+//     i++;
+// }
+
+
+
+// template literals
+
+// `    ${}`
---
+++ New folder/day6.js
@@ -0,0 +1,75 @@
+//Dom:
+//garb
+// 5methods
+//3methods
+// getElementById()
+//getElementsByClassName()
+//getElementsByTagName();
+
+
+
+// const head = document.getElementById('head');
+// console.log(head);
+//https://images.unsplash.com/photo-1685440210801-d66177b55a06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60
+//class Tag;
+
+// const comp = document.getElementsByClassName('com')
+// console.log(comp)
+
+// let  i =0;
+
+// while( i < comp.length){
+//     console.log(comp[i])
+//     i++
+// }
+
+// comp[2].addEventListener('click',function(){
+//    const img = document.querySelector('img')
+
+
+//    img.src='https://images.unsplash.com/photo-1685440210801-d66177b55a06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+   
+//   img.setAttribute('src','https://images.unsplash.com/photo-1685440210801-d66177b55a06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
+// })
+
+//2methods:Qyeryselectors
+// const component = document.querySelectorAll('.com')
+// console.log(component)
+
+// component.forEach(function(value){
+//     console.log(value)
+// })
+
+
+//change a element:
+
+//content - innerHTML
+//style
+//src;
+
+
+
+
+// Array iterator
+
+// const arr = [1,2,34,56,5];
+
+
+// let i =0;
+
+// while(i<arr.length){
+//     console.log(arr[i]*2);
+//     i++
+// }
+
+
+//for Each 
+
+
+// arr.forEach(function(value,index,array){
+//    console.log(value*2)
+// });
+
+
+
+// methods , operators , Dom nodes
