e2943a7bd2f98c89093af9f4be73d14544f029d2
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-06-05 10:04:18 +0530

    Add files via upload

---
+++ New folder/ES6/day1.js
@@ -0,0 +1,45 @@
+//ECMA SCRIPT - GOVERNING BOD FOR JS.
+
+//RULE AND REGULATION. 
+
+//JS ENGINE BROWESER - SIMILAR OUTPUT IN SIMIAAR,
+
+
+// ES6  - MAJOR MODERN JS 
+
+// LET CONST, MAP
+
+//LET CONST VAR .
+
+
+
+
+//SCOPING - Accesing the varibale and function;
+
+// lexical scoping - based on wherre the varible are a function is created their accessiblity is determing
+
+//global , function , block 
+
+
+// let  a=10; //global           - all over the page
+
+
+// function b(){
+//     let c =12; //function scope  - access only inside the functionn.
+
+   
+// }
+
+// b()
+
+
+// {
+//     var d =12; //block          - var , ;let
+
+// }
+// console.log(d)
+
+// 2 difference scope - hositing.
+
+
+
