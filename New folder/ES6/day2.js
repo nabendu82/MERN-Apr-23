2fade6028428f54c626dc6b7319d0ee943690eb3
K_Hari_Prasath <93816270+khari-on@users.noreply.github.com>
2023-06-12 09:30:54 +0530

    Add files via upload

---
+++ New folder/ES6/day2.js
@@ -0,0 +1,48 @@
+//iteration.
+
+// for each
+
+
+const arr = [1,2,3,4,5,6,7,];
+
+// 'The fruits left in the shop are '+
+// const arr1=[]
+
+// arr.forEach(function(value,index,array){
+//   arr1.push('fruit - '+ value)
+// })
+
+// console.log(arr1)
+
+
+//map , filter ,reduce;
+
+// const arr1 =arr.map(function(value, index, array){
+
+
+// })
+
+
+// console.log(arr1)
+
+//filter:
+
+// const arr1 =arr.filter(function(value, index, array){
+//     return value  > 3
+// })
+
+
+// console.log(arr1)
+
+
+// const arr1 =arr.reduce(function(accum,cumm, index, array){
+
+// return accum+cumm;
+// },8)
+
+
+// console.log(arr1)
+
+
+
+//array methods ; some every find 
---
+++ New folder/ES6/day3.js
@@ -0,0 +1,150 @@
+//destructing;
+
+// const arr = [1,2,3,4,5]
+
+// const item1 = arr[0];
+
+// // console.log(item1)
+
+// const item3 = arr[2];
+
+// console.log(item3)
+
+
+// const [item1,item2,,,item5] =arr;
+
+// console.log(item1,item2,item5)
+
+
+// obj
+
+// const person = {
+//     name:'John',
+//     age:25,
+
+
+//     details:function(){
+//         return person.name + ' this is my name'
+//     }
+// }
+
+
+// old
+
+// const personName = person.name;
+
+// console.log(personName)
+
+
+// const {name:personName,age,details}=person;
+
+// console.log(details())
+
+
+// const arrItem = [
+//     {
+//         name:'Vikram',
+//         skils:[1,2,3]
+//     }
+// ]
+
+
+// oldways
+
+// const [item1]    =arrItem //destructing a array -1
+
+// const  {name,skils}=item1//destructing a object -2
+
+
+// short syntax
+
+// const [{name,skils}]=arrItem;
+
+// console.log(name,skils)
+
+
+//arrow function 
+
+//2things syntax reduced
+
+//Declartion types:
+
+
+// function name(){
+//     return 
+// };
+
+
+//expression types:
+
+// const personDetail = function(){
+//   return 'hi'
+// }
+
+// const firstname = 'john'
+
+
+// console.log(personDetail())
+
+
+// ()=>
+
+// const personInfo = a => {
+//     return a
+// } 
+
+// const personInfo = (a,b) =>a+b
+
+// const personInfo= (a,b)=>{
+//     let c = a+b;
+//     return c;
+// }
+
+// console.log(personInfo(2,7))
+
+
+// this keyword.
+//classes
+
+
+//this
+
+
+//spreed, rest. - ...
+
+// spreed = list , copy indiviual values
+
+// const arr =  [1,2,3]
+
+
+
+// const person = {
+//     name:'john',
+//     age:35
+// }
+
+
+// const person1 = {
+//     age:89,
+//     color:'brown'
+// }
+
+
+// const person3 = {...person,...person1}
+
+
+// console.log(person3)
+
+
+
+// const arr1 = [1,2,3,4,5,6,7,8];
+
+// const [item1,item2,...rest]=arr1
+
+// console.log(item1,item2,rest)
+
+
+
+
+
+
