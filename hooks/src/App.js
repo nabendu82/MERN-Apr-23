17675657c3a2974fa52a4928de63cc367efd71f2
nabendu82 <nabendu.biswas@gmail.com>
2023-07-07 21:33:17 +0530

    Hooks started

---
+++ hooks/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HookObj from './components/HookObj';
+
+function App() {
+  return (
+    <div className="App">
+      <HookObj />
+    </div>
+  );
+}
+
+export default App;
