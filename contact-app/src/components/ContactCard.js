faa4f9325ef552e7237b6939f11e86b28491a386
nabendu82 <nabendu.biswas@gmail.com>
2023-07-18 21:30:41 +0530

    contact app done

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -1,7 +1,7 @@
 import user from "../images/user.png"
 
 const ContactCard = (props) => {
-    const { name, email } = props.contact;
+    const { id, name, email } = props.contact;
 
     return (
         <div className="item">
@@ -10,7 +10,11 @@ const ContactCard = (props) => {
                 <div className="header">{name}</div>
                 <div>{email}</div>
             </div>
-            <i className="trash alternate outline icon" style={{ color: "red" }}></i>
+            <i 
+                className="trash alternate outline icon" 
+                style={{ color: "red" }}
+                onClick={() => props.clickHandler(id)}
+            ></i>
         </div>
     )
 }
--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard"
 const ContactList = (props) => {
     return (
         <div className="ui celled list">
-            {props.contacts.map(contact =>  <ContactCard key={contact.id} contact={contact} />)}
+            {props.contacts.map(contact =>  <ContactCard key={contact.id} contact={contact} clickHandler={id => props.getContactId(id)} />)}
         </div>
     )
 }
